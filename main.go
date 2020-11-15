6798d236b86ea5fd5124043b7efa88ca5c0fcacd
Simo Aleksandrov <simo3003@me.com>
2020-11-15 03:30:31 +0200

    Render object origins

--- main.go
+++ main.go
@@ -91,9 +91,8 @@ func main() {
 
 		car.Update(dt.Seconds())
 
-		x := int(car.X / (128 * 2))
-		y := int(car.Y / (128 * 2))
-		currentTile := level.Layers[1].Tiles[y*level.Width+x]
+		x := int(car.X / (128 * 2)) // 2 is the map scale, 128 is the tile size
+		y := int(car.Y / (128 * 2)) // 2 is the map scale, 128 is the tile size
 
 		camera.Target = car.Vector2
 
@@ -102,15 +101,47 @@ func main() {
 
 		rl.BeginMode2D(camera)
 
-		rl.DrawTextureEx(bg, rl.Vector2{0, 0}, 0, 2, rl.White)
+		rl.DrawTextureEx(bg, rl.Vector2{X: 0, Y: 0}, 0, 2, rl.White)
 		car.Draw()
+
+		currentTile := level.Layers[1].Tiles[y*level.Width+x]
+		for _, objGroup := range level.Tilesets[0].Tiles[currentTile.ID].ObjectGroups {
+			for _, obj := range objGroup.Objects {
+				// We don't care about this object if it's a property
+				if len(obj.Properties) != 0 {
+					continue
+				}
+				start := rl.Vector2{X: (float32(obj.X) + float32(x)*128) * 2, Y: (float32(obj.Y) + float32(y)*128) * 2}
+				rl.DrawCircleV(start, 10, rl.Pink)
+
+				if len(obj.Ellipses) == 1 {
+					// TODO: render Ellipse
+				}
+
+				if len(obj.PolyLines) == 1 {
+					// TODO: render PolyLine
+				}
+
+				if len(obj.Polygons) == 1 {
+					// for _, point := range *obj.Polygons[0].Points {
+					// 	end := rl.Vector2{X: float32(point.X) + float32(x)*128*2, Y: float32(point.Y) + float32(y)*128*2}
+					// 	rl.DrawLineV(start, end, rl.Gray)
+					// 	start = end
+					// }
+
+					// rl.DrawLineV(origin, rl.Vector2{float32(points[0].X) + float32(x)*128*2, float32(points[0].Y) + float32(y)*128*2}, rl.Gray)
+					// rl.Draw
+				}
+			}
+		}
 
 		rl.EndMode2D()
 
 		rl.DrawText("Press R to restart", 5, 5, 25, rl.Black)
 		rl.DrawText("Use WASD to move", 5, 35, 25, rl.Black)
 		rl.DrawText(fmt.Sprintf("%d", int(car.Speed)), 1024-80, 768-50, 35, rl.Black)
-		rl.DrawText(fmt.Sprintf("currentTile.ID = %d", currentTile.ID+1), 5, 768-50, 35, rl.Black)
+		rl.DrawText(fmt.Sprintf("%d,%d", x, y), 5, 768-50, 35, rl.Black)
+		// rl.DrawText(fmt.Sprintf("currentTile.ID = %d", currentTile.ID), 5, 768-50, 35, rl.Black)
 
 		rl.EndDrawing()
 	}
